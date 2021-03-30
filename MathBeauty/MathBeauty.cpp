d33374f6aab3d55847def14dc7a41c000bddc6b8
petialetia <70401862+petialetia@users.noreply.github.com>
2021-03-30 05:39:12 +0300

    Add files via upload

--- MathBeauty/MathBeauty.cpp
+++ MathBeauty/MathBeauty.cpp
@@ -1,22 +1,27 @@
 #include "TXLib.h"
 
-const int SCREEN_LENGTH = 1501;
-const int SCREEN_WIDTH  = 1001;
+const int SCREEN_LENGTH = 1200;
+const int SCREEN_WIDTH  = 1000;
 
-const float CENTER_PIXEL_X = 1001.f;
-const float CENTER_PIXEL_Y = 501.f;
+const float CENTER_PIXEL_X = 800.f;
+const float CENTER_PIXEL_Y = 500.f;
 
-const float ZOOM_PIXEL_X = 300.f;
-const float ZOOM_PIXEL_Y = 501.f;
+const float ZOOM_PIXEL_X = 99.f;
+const float ZOOM_PIXEL_Y = 500.f;
 
 const float START_SCALE = 500;
 
-const int SCALE_INCREASING = 1;
+const int SCALE_INCREASING = 2;
+
+const float MAX_SCALE = 10000000;
+
+const float MAX_R = 1000;
+
+const int MAX_COUNTER = 510;
 
 int main ()
 {
     txCreateWindow (SCREEN_LENGTH, SCREEN_WIDTH);
-    //txBegin ();
 
     typedef RGBQUAD (&scr_t) [SCREEN_WIDTH][SCREEN_LENGTH];
     scr_t screen = (scr_t) *txVideoMemory ();
@@ -24,42 +29,42 @@ int main ()
     float zoom_coordinate_x = (ZOOM_PIXEL_X - CENTER_PIXEL_X) / START_SCALE;
     float zoom_coordinate_y = (ZOOM_PIXEL_Y - CENTER_PIXEL_Y) / START_SCALE;
 
-    for (float current_scale = START_SCALE; current_scale < 10000000; current_scale *= SCALE_INCREASING)
+    for (float current_scale = START_SCALE; current_scale < MAX_SCALE; current_scale *= SCALE_INCREASING)
     {
         if (GetAsyncKeyState (VK_ESCAPE)) break;
 
+        float step_x = 1/current_scale;
+        float step_y = 1/current_scale;
 
-        float dx = 1/current_scale;
-        float dy = 1/current_scale;
+        float start_y = zoom_coordinate_y - ZOOM_PIXEL_Y / current_scale;
 
-        float y0 = zoom_coordinate_y - ZOOM_PIXEL_Y / current_scale;
-
-        for (int i_y = 0; i_y < SCREEN_WIDTH; i_y++, y0 += dy)
+        for (int i_y = 0; i_y < SCREEN_WIDTH; i_y++, start_y += step_y)
         {
-            float x0 = zoom_coordinate_x - ZOOM_PIXEL_X / current_scale;
+            float start_x = zoom_coordinate_x - ZOOM_PIXEL_X / current_scale;
 
-            for (int i_x = 0; i_x < SCREEN_LENGTH; i_x++, x0 += dx)
+            for (int i_x = 0; i_x < SCREEN_LENGTH; i_x++, start_x += step_x)
             {
-                float x = x0;
-                float y = y0;
+                float current_x = start_x;
+                float current_y = start_y;
 
                 int i = 0;
 
-                for (; i < 510; i++)
+                for (; i < MAX_COUNTER; i++)
                 {
-                    float x2 = x*x,
-                          y2 = y*y,
-                          xy = x*y;
+                    float x_squared = current_x*current_x,
+                          y_squared = current_y*current_y,
+                          xy = current_x*current_y;
 
-                    float r2 = x2 + y2;
+                    float r_squared = x_squared + y_squared;
 
-                    if (r2 >= 1000) break;
+                    if (r_squared >= MAX_R) break;
 
-                    x = x2 - y2 + x0;
-                    y = xy + xy + y0;
+                    current_x = x_squared - y_squared + start_x;
+                    current_y = xy + xy + start_y;
                 }
 
-                screen[i_y][i_x] = {(i%255) * i / 255, i%255, 0};
+                screen[i_y][i_x] = { (i%255) * i / 255, i%255, 0};
+                //screen[i_y][i_x] = {i, i, i};
             }
         }
 
