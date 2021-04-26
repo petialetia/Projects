2cbb7003b8bd7fb656b5ac9e2981972f2fcf3dc8
petialetia <70401862+petialetia@users.noreply.github.com>
2021-04-26 10:54:12 +0300

    Delete src directory

--- src/StandartComparator.asm
+++
@@ -1,9 +0,0 @@
-global StandartComparator
-section .text
-
-StandartComparator:
-
-    vpcmpeqb ymm0, ymm1, ymm0 ; comparing 2 vectors
-    vpmovmskb eax, ymm0       ; moving results of comparison in eax
-
-    ret   
